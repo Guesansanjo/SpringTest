a9048ebf754a2e31b20aa536a3618d5c2799d1e4
Jose Guerrero <guesanjoupv@gmail.com>
2023-03-04 17:05:02 +0100

    setup
    Controller/Repository/Service

---
+++ demo/src/main/java/com/example/demo/Service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.Service.impl;
+
+import com.example.demo.Service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
