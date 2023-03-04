0618239565cf3d9b7b5802976e7a93ca8c9b9e33
Jose Guerrero <guesanjoupv@gmail.com>
2023-03-04 17:37:10 +0100

    feat : ADD post
    method functional tested

--- demo/src/main/java/com/example/demo/Service/impl/UserServiceImpl.java
+++ demo/src/main/java/com/example/demo/Service/impl/UserServiceImpl.java
@@ -1,8 +1,22 @@
 package com.example.demo.Service.impl;
 
 import com.example.demo.Service.UserService;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void addUSer(User user) {
+    userRepository.save(user);
+    }
 }
--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.Service.UserService;
+import com.example.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,12 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/add")
+    public String addUser(@RequestBody User user){
+        userService.addUSer(user);
+
+        return "Success";
     }
 }
